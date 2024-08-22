6178ab96ae671ca2785119f286039b933759b427
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-22 17:47:22 +0000

    add overview os performance metrics in ml performance page

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -40,5 +40,18 @@ def page_ml_performance():
         st.image(model_loss, caption='Model Training Losses')
     st.write("---")
 
+    st.write('### Overview of Performance Metrics')
+    st.info(
+        f"Following an extensive evaluation of the model's performance on both the training and validation datasets, it can be confidently stated "
+        f'that the CNN model developed for classifying cherry leaves is performing at a high level of accuracy, as expected.\n\n'
+        f'*Overfitting has been kept to a minimum,* which indicates that the model is likely to generalize effectively to new, unseen data.\n\n'
+        f'The model was trained over several epochs (training cycles), during which it achieved low loss values and high accuracy. '
+        f'The accuracy metrics demonstrate a consistent upward trajectory, reflecting the model’s improved ability to differentiate between healthy leaves and those affected by powdery mildew over time. '
+        f'Although the accuracy lines for training and validation sets are not exactly aligned, they remain closely associated throughout the training process, with only minor deviations.\n\n'
+        f'Additionally, the loss metrics show a steady decline, indicating that the model is progressively reducing errors. '
+        f'The loss lines for both the training and validation datasets closely follow one another, especially in the later stages of training, where they converge, suggesting minimal differences between the datasets.\n\n'
+        f'Overall, the accuracy and loss trends demonstrate that the model is learning effectively, with accuracy improving and loss decreasing, confirming that the model is robust and capable of performing well on unseen data.'
+    )
+
     st.write("### Generalised Performance on Test Set")
     st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
