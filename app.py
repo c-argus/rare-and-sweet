59851ed3f2f900d604ef54eb62d1c3e705f751ca
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-22 09:36:28 +0000

    Small format changes in summary page

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_image_visualizer import page_image_visualizer_body
 from app_pages.page_cherry_leaves_detector import page_cherry_leaves_detector
 from app_pages.page_model_training import page_model_training_body
-from app_pages.page_model_evaluation import page_model_evaluation_body
+# from app_pages.page_model_evaluation import page_model_evaluation_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Cherry Leaves Disease Detector")
 
 # Add app pages
 app.add_page("Project Summary", page_summary_body)
-app.add_page("Image Visualizer", page_image_visualizer_body)
-app.add_page("Image Analysis", page_image_analysis_body)
-app.add_page("Model Training", page_model_training_body)
-app.add_page("Model Evaluation", page_model_evaluation_body)
+# app.add_page("Image Visualizer", page_image_visualizer_body)
+# app.add_page("Image Analysis", page_image_analysis_body)
+# app.add_page("Model Training", page_model_training_body)
+# app.add_page("Model Evaluation", page_model_evaluation_body)
 
 # Run the app
 app.run()
--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -5,57 +5,59 @@ import matplotlib as plt
 def page_summary_body():
 
     # Displaying the project title and introduction
-    st.title('## Project Summary')
+    st.title('Project Summary')
     st.write('### Introduction ')
     st.write('#### Powdery Mildew Detector on Cherry Trees')
 
     st.write(
         f'Powdery Mildew is a widespread fungal infection that impacts a variety of plants, '
         f'visibly identified by light grey or white powdery spots that primarily appear on leaves, '
-        f'but can also affect stems, flowers, and fruits. As the disease progresses, these spots '
+        f'but can also affect stems, flowers, and fruits. '
+        f'As the disease progresses, these spots '
         f'can expand, covering large portions of the leaves, particularly on newer growth.\n\n'
 
         f'While the disease is seldom fatal, if left untreated, powdery mildew can cause significant '
         f'damage by robbing the plant of essential water and nutrients. Common symptoms include yellowing, '
         f'withering, leaf distortion, reduced growth, fewer flowers, and delayed development.\n\n'
 
-        f'The current method of manually inspecting each **cherry tree** is time-consuming, taking roughly '
+        f'The current method of manually inspecting each cherry tree is time-consuming, taking roughly '
         f'30 minutes per tree, plus an additional minute for treatment if required. Given the vast number of '
-        f'trees spread across various farms, this approach is not scalable. To overcome this challenge, the IT team '
+        f'trees spread across various farms, this approach is not scalable. To overcome this challenge, this project '
         f'has proposed the implementation of a machine learning (ML) system that can quickly detect powdery mildew in '
-        f'images of **cherry trees**. If successful, this solution could be extended to other crops.\n\n'
+        f'images of cherry trees.\n\n'
     )
 
-# Define the file paths to example images of healthy and powdery mildew leaves
-image_paths = [
+    # Define the file paths to example images of healthy and powdery mildew leaves
+    image_paths = [
         'inputs/cherry-leaves_dataset/cherry-leaves/validation/healthy/40dce93a-7697-4c0d-b37d-e3147c7ece78___JR_HL 9464.JPG',
         'inputs/cherry-leaves_dataset/cherry-leaves/validation/powdery_mildew/26cdf0b5-906d-4341-a07b-d8d9ec21cda7___FREC_Pwd.M 5151.JPG'
     ]
 
-# Define captions for the images to explain what each image represents
-captions = ["Healthy Leaf Example", "Powdery Mildew Leaf Example"]
+    # Define captions for the images to explain what each image represents
+    captions = ["Healthy Leaf Example", "Powdery Mildew Leaf Example"]
 
-# Display the images side by side in the Streamlit app with captions
-st.image(image_paths, caption=captions)
+    # Display the images side by side in the Streamlit app with captions
+    st.image(image_paths, caption=captions)
 
-# Provide links for additional information and references
-st.info(
+    # Provide links for additional information and references
+    st.info(
         f'For further information:\n'
         f'* Refer to the [Project README file](https://github.com/c-argus/rare-and-sweet/blob/main/README.md).\n'
     )
 
-# Summarizing the project's business objectives
-st.write(
-    f'## The project focuses on two key business objectives:\n\n'
-    f'* 1- To visually differentiate a healthy cherry leaf from one with powdery mildew.\n' 
-    f'* 2- To predict whether a cherry tree is healthy or suffering from powdery mildew.' 
-)
+    # Summarizing the project's business objectives
+    st.write(
+        f'### Objectives:\n\n'
+        f'The project focuses on two key business:\n'
+        f'* 1- To visually differentiate a healthy cherry leaf from one with powdery mildew.\n' 
+        f'* 2- To predict whether a cherry tree is healthy or suffering from powdery mildew.' 
+    )
 
-# Briefly describing the project dataset
-st.write('### Project Dataset:')
-st.write(
-    f'The dataset, consisting of cherry tree leaf images provided by Farmy & Foods, '
-    f'is the foundation for training and evaluating the model.\n\n'
-    f'This dataset is available on [Kaggle](https://www.kaggle.com/codeinstitute/cherry-leaves) and '
-    f'includes over 4,000 images, with a selected subset used for efficient model training.'
-)
+    # Briefly describing the project dataset
+    st.write('### Project Dataset:')
+    st.write(
+        f'The dataset, consisting of cherry tree leaf images provided by Farmy & Foods, '
+        f'is the foundation for training and evaluating the model.\n'
+        f'This dataset is available on [Kaggle](https://www.kaggle.com/codeinstitute/cherry-leaves) and '
+        f'includes over 4,000 images, with a selected subset used for efficient model training.'
+    )
