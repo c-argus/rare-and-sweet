421a46029a5c564158e97f49b8fc13c18834c538
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-22 15:06:11 +0000

    add data management page

--- src/data_management.py
+++ src/data_management.py
@@ -1,20 +1,27 @@
-import pandas as pd
-import streamlit as st
+import numpy as np 
+import pandas as pd 
+import os 
 import base64
+from datetime import datetime 
 import joblib
 
 
-def download_dataframe_as_csv(df: pd.DataFrame, filename="data.csv"):
+def download_dataframe_as_csv(df):
     """
     Generates a link to download the given DataFrame as a CSV file.
     """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV file</a>'
+    daytime_now = datetime.now().strftime("%d%b%Y_%Hh%Mmin%Ss")
+    csv = df.to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = (
+        f' <a href="data:file/csv;base64,{b64}" download="Report {daytime_now}.csv" '
+        f'target="_blank">Download Report </a>'
+    )
     return href
 
-def load_pkl_file(file_path: str):
-    """
-    Load a pickle file from the given path.
-    """
-    return joblib.load(file_path)
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
+
+
+    
+
