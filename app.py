9ccb808e5d162d83aeaa117019969a22cb9ff018
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-22 15:51:58 +0000

    add page hypothesis

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_image_visualizer import page_image_visualizer_body
 from app_pages.page_cherry_leaves_detector import page_cherry_leaves_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-# from app_pages.page_ml_performance import page_ml_performance_body
+from app_pages.page_ml_performance import page_ml_performance_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Cherry Leaves Disease Detector")
--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
-def page_model_training_body():
-    st.title("Model Training")
-    st.write("This page will guide you through the model training process.")
+# Function to display the Project Insights and Findings page
+def page_project_hypothesis_body():
+    st.title("Project Hypothesis and Validation")
+
+    st.info(
+        f'Plants suffering from a fungal infection, particularly powdery mildew, '
+        f'display unique visual traits on their leaves, ' 
+        f'including pale-grey or white powdery spots.'
+    )
+
+    # Displaying a success message with the validation of the observation
+    st.success(
+        f'### Validation:\n\n'
+        f'An image collage displays the typical whitish spots on leaves impacted '
+        f'by a powdery mildew fungal infection. '
+        f'By examining the average images, variability images, and the contrasts between these averages, '
+        f'clear patterns become evident, aiding in the distinction between infected and healthy leaves.'
+    )
+
+
